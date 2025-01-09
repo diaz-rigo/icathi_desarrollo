c21bc3320a26cdc3ac35ee0fb6dc57ed86dd1e56
diaz-rigo <morelosalfaro@gmail.com>
2025-01-09 04:49:45 -0600

    Update detalle-plantel.component.ts

--- src/app/modules/public/pages/plantel/detalle-plantel/detalle-plantel.component.ts
+++ src/app/modules/public/pages/plantel/detalle-plantel/detalle-plantel.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-detalle-plantel',
-  standalone: true,
-  imports: [],
-  templateUrl: './detalle-plantel.component.html',
-  styleUrl: './detalle-plantel.component.scss'
+    selector: 'app-detalle-plantel',
+    imports: [],
+    templateUrl: './detalle-plantel.component.html',
+    styleUrl: './detalle-plantel.component.scss'
 })
 export class DetallePlantelComponent {
 
