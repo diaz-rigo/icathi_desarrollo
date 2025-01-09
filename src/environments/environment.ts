c705186684721a09e348d59d5071f025f546a7e1
diaz-rigo <morelosalfaro@gmail.com>
2025-01-09 04:49:53 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: false
+
   //  ,api:'http://localhost:3000'
-  ,api:'https://iicathi-api.onrender.com'
+   ,api:'https://iicathi-api.onrender.com'
+
 
 };
 
