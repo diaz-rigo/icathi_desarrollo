3828899f39aa270b6853aaeea2570688680ab5b7
diaz-rigo <morelosalfaro@gmail.com>
2025-01-09 04:49:39 -0600

    Add curso-modalidad-escuela.component.ts

---
+++ src/app/modules/private/pages/cursos/typos-cursos/curso-modalidad-escuela/curso-modalidad-escuela.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-curso-modalidad-escuela',
+  templateUrl: './curso-modalidad-escuela.component.html',
+  styles: ``
+})
+export class CursoModalidadEscuelaComponent {
+
+}
