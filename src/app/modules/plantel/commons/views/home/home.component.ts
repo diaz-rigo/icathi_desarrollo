d6919c312db223961f9a8510cfc53db9cc85a5f9
diaz-rigo <morelosalfaro@gmail.com>
2025-01-09 04:49:12 -0600

    Update home.component.ts

--- src/app/modules/plantel/commons/views/home/home.component.ts
+++ src/app/modules/plantel/commons/views/home/home.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../../../../environments/environment.prod';
+import { AuthService } from '../../../../../shared/services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styles: ``,
-  standalone:false,
-
+  standalone: false,
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  data: any = {};
+  loading:boolean=false
+  constructor(private http: HttpClient, private auth: AuthService) {}
+  
+  ngOnInit(): void {
+    this.loading=true
+    this.auth.getIdFromToken().then((idPlantel) => {
+      this.http
+        .get<any>(`${environment.api}/plantelesCursos/info/${idPlantel}`)
+        .subscribe((response) => {
+          this.data= response;
+      this.loading=false
+        });
+    });
+  }
 }
