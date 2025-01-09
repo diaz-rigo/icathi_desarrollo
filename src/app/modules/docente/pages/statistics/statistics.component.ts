3f8ad56b20bd87a5d366edd73bd44c1246d60dcc
diaz-rigo <morelosalfaro@gmail.com>
2025-01-09 04:49:01 -0600

    Update statistics.component.ts

--- src/app/modules/docente/pages/statistics/statistics.component.ts
+++ src/app/modules/docente/pages/statistics/statistics.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-statistics',
-  templateUrl: './statistics.component.html',
-  styleUrl: './statistics.component.scss'
+    selector: 'app-statistics',
+    templateUrl: './statistics.component.html',
+    styleUrl: './statistics.component.scss',
+    standalone: false
 })
 export class StatisticsComponent {
 
