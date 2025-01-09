8de2e747f266bcd0461c5430142448263a8311f4
diaz-rigo <morelosalfaro@gmail.com>
2025-01-09 04:49:49 -0600

    Update validador-plantel.component.ts

--- src/app/modules/validador/pages/validador-plantel/validador-plantel.component.ts
+++ src/app/modules/validador/pages/validador-plantel/validador-plantel.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-validador-plantel',
-  templateUrl: './validador-plantel.component.html',
-  styleUrl: './validador-plantel.component.scss'
+    selector: 'app-validador-plantel',
+    templateUrl: './validador-plantel.component.html',
+    styleUrl: './validador-plantel.component.scss',
+    standalone: false
 })
 export class ValidadorPlantelComponent {
   plantelData: any = {};
