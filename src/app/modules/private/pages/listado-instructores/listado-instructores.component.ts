5f4895bd7026672ea917ab0691b58f1778523ebb
diaz-rigo <morelosalfaro@gmail.com>
2025-01-09 01:35:33 -0600

    Update listado-instructores.component.ts

--- src/app/modules/private/pages/listado-instructores/listado-instructores.component.ts
+++ src/app/modules/private/pages/listado-instructores/listado-instructores.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-listado-instructores',
-    templateUrl: './listado-instructores.component.html',
-    styleUrl: './listado-instructores.component.scss',
-    standalone: false
+  selector: 'app-listado-instructores',
+  templateUrl: './listado-instructores.component.html',
+  styleUrl: './listado-instructores.component.scss'
 })
 export class ListadoInstructoresComponent {
 
