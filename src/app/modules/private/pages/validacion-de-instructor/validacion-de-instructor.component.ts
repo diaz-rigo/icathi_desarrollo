c73ad7a231f661e89af2a6cea3b381543c943a14
diaz-rigo <morelosalfaro@gmail.com>
2025-01-09 04:49:21 -0600

    Update validacion-de-instructor.component.ts

--- src/app/modules/private/pages/validacion-de-instructor/validacion-de-instructor.component.ts
+++ src/app/modules/private/pages/validacion-de-instructor/validacion-de-instructor.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-validacion-de-instructor',
-  templateUrl: './validacion-de-instructor.component.html',
-  styleUrl: './validacion-de-instructor.component.scss'
+    selector: 'app-validacion-de-instructor',
+    templateUrl: './validacion-de-instructor.component.html',
+    styleUrl: './validacion-de-instructor.component.scss',
+    standalone: false
 })
 export class ValidacionDeInstructorComponent {
 
