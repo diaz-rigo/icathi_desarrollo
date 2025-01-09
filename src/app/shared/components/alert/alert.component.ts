f707c49b83e1a4937bbb8c698013d60fcbc18ca3
diaz-rigo <morelosalfaro@gmail.com>
2025-01-09 04:49:50 -0600

    Update alert.component.ts

--- src/app/shared/components/alert/alert.component.ts
+++ src/app/shared/components/alert/alert.component.ts
@@ -3,19 +3,20 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
 import { CommonModule } from '@angular/common';
 
 @Component({
-  // standalone: true,
-  // imports: [CommonModule],
-  selector: 'app-alert',
-  templateUrl: './alert.component.html',
-  styleUrls: ['./alert.component.scss'],
-  animations: [
-    trigger('slideInFadeOut', [
-      state('in', style({ opacity: 1, transform: 'translateX(0)' })),
-      state('out', style({ opacity: 0, transform: 'translateX(100%)' })),
-      transition('in => out', [animate('300ms ease-out')]),
-      transition('out => in', [animate('300ms ease-in')]),
-    ]),
-  ],
+    // standalone: true,
+    // imports: [CommonModule],
+    selector: 'app-alert',
+    templateUrl: './alert.component.html',
+    styleUrls: ['./alert.component.scss'],
+    animations: [
+        trigger('slideInFadeOut', [
+            state('in', style({ opacity: 1, transform: 'translateX(0)' })),
+            state('out', style({ opacity: 0, transform: 'translateX(100%)' })),
+            transition('in => out', [animate('300ms ease-out')]),
+            transition('out => in', [animate('300ms ease-in')]),
+        ]),
+    ],
+    standalone: false
 })
 export class AlertComponent {
   @Input() message: string = '';
