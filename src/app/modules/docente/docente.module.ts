dbe7dd4dfb18a8769cf446663f99bf3e49c61233
diaz-rigo <morelosalfaro@gmail.com>
2025-01-09 04:48:59 -0600

    Update docente.module.ts

--- src/app/modules/docente/docente.module.ts
+++ src/app/modules/docente/docente.module.ts
@@ -9,6 +9,9 @@ import { StatisticsComponent } from './pages/statistics/statistics.component';
 import { FormsModule } from '@angular/forms';
 import { NbThemeModule,NbCardModule, NbSelectModule } from '@nebular/theme';
 import { SafeUrlPipe } from '../../shared/pipes/safe-url.pipe';
+import { AlumnosCursosComponent } from './pages/alumnos/alumnos-cursos/alumnos-cursos.component';
+import { CursosComponent } from './pages/cursos/cursos.component';
+import { AistenciasComponent } from './pages/aistencias/aistencias.component';
 
 
 @NgModule({
@@ -16,7 +19,10 @@ import { SafeUrlPipe } from '../../shared/pipes/safe-url.pipe';
     DocenteComponent,
     ProfileComponent,
     CoursesComponent,
-    StatisticsComponent
+    StatisticsComponent,
+    AlumnosCursosComponent,
+    CursosComponent,
+    AistenciasComponent
   ],
   imports: [
     NbThemeModule.forRoot(),
