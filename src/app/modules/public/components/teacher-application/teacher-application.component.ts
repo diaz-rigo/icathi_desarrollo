01452c82fbd5688c56701bf99c853a25b13684ac
diaz-rigo <morelosalfaro@gmail.com>
2025-01-09 04:49:22 -0600

    Update teacher-application.component.ts

--- src/app/modules/public/components/teacher-application/teacher-application.component.ts
+++ src/app/modules/public/components/teacher-application/teacher-application.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-teacher-application',
-  templateUrl: './teacher-application.component.html',
-  styleUrl: './teacher-application.component.scss'
+    selector: 'app-teacher-application',
+    templateUrl: './teacher-application.component.html',
+    styleUrl: './teacher-application.component.scss',
+    standalone: false
 })
 export class TeacherApplicationComponent {
 
