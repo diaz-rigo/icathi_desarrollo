d045510f320556a2c3cd74b0f736d236538967fc
diaz-rigo <morelosalfaro@gmail.com>
2025-01-09 04:49:51 -0600

    Update aspirante.service.ts

--- src/app/shared/services/aspirante.service.ts
+++ src/app/shared/services/aspirante.service.ts
@@ -4,10 +4,9 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AspiranteService {
-
   private apiUrl = `${environment.api}/aspirante/registro`;
 
   constructor(private http: HttpClient) {}
@@ -17,8 +16,15 @@ export class AspiranteService {
     return this.http.post(this.apiUrl, JSON.stringify(data), { headers });
   }
 
-  getApirantes():Observable<any>{
-    return this.http.get(`${environment.api}/alumno`)
+  registrarAspiranteByPllantel(data: any): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post(`${environment.api}/aspirante/registro/${data.plantel}`, JSON.stringify(data), { headers });
+  }
+
+
+
+  getApirantes(): Observable<any> {
+    return this.http.get(`${environment.api}/alumno`);
   }
 
   // Obtener un alumno por su ID
@@ -26,18 +32,17 @@ export class AspiranteService {
     return this.http.get(`${environment.api}/alumno/usuario/${id}`);
   }
 
-
-
-  getApirantesBIdPlantel(id:any):Observable<any>{
+  getApirantesBIdPlantel(id: any): Observable<any> {
     // http://localhost:3000/alumnosPlantelCursos/byIdPlantel/3
-    return this.http.get(`${environment.api}/alumnosPlantelCursos/byIdPlantel/${id}`)
+    return this.http.get(
+      `${environment.api}/PlantelCursos/byIdPlantel/${id}/alumnos`
+    );
   }
-// Nueva función para obtener el CURP por correo electrónico
-obtenerCurpPorEmail(email: string): Observable<any> {
-  return this.http.get(`${environment.api}/aspirante/curp/${email}`);
-}  // Nueva función para obtener un alumno por su ID
-getAlumnoById(id: number): Observable<any> {
-  return this.http.get(`${environment.api}/alumno/${id}`);
-}
-
+  // Nueva función para obtener el CURP por correo electrónico
+  obtenerCurpPorEmail(email: string): Observable<any> {
+    return this.http.get(`${environment.api}/aspirante/curp/${email}`);
+  } // Nueva función para obtener un alumno por su ID
+  getAlumnoById(id: number): Observable<any> {
+    return this.http.get(`${environment.api}/alumno/${id}`);
+  }
 }
