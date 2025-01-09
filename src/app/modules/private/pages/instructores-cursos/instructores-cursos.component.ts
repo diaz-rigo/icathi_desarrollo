288a71330b2ddc8e66eab121b84b6d9107912927
diaz-rigo <morelosalfaro@gmail.com>
2025-01-09 04:49:17 -0600

    Update instructores-cursos.component.ts

--- src/app/modules/private/pages/instructores-cursos/instructores-cursos.component.ts
+++ src/app/modules/private/pages/instructores-cursos/instructores-cursos.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-instructores-cursos',
-  templateUrl: './instructores-cursos.component.html',
-  styleUrl: './instructores-cursos.component.scss'
+    selector: 'app-instructores-cursos',
+    templateUrl: './instructores-cursos.component.html',
+    styleUrl: './instructores-cursos.component.scss',
+    standalone: false
 })
 export class InstructoresCursosComponent {
 
