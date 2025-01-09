54f63e021458e60e20d6e7052f1b8ac1cfe194b1
diaz-rigo <morelosalfaro@gmail.com>
2025-01-09 04:49:21 -0600

    Update private-routing.module.ts

--- src/app/modules/private/private-routing.module.ts
+++ src/app/modules/private/private-routing.module.ts
@@ -12,6 +12,7 @@ import { FrmPlantelComponent } from './pages/planteles/frm-plantel/frm-plantel.c
 import { ListadoPlantelesComponent } from './pages/planteles/listado-planteles/listado-planteles.component';
 import { DocentesComponent } from './pages/docentes/docentes.component';
 // import { ListadoCursosComponent } from './pages/listado-cursos/listado-cursos.component';
+import { PdfExampleComponent } from './pages/pdf-example/pdf-example.component';
 
 const routes: Routes = [
   {
@@ -29,6 +30,10 @@ const routes: Routes = [
       {
         path: 'lista-planteles',
         component: ListadoPlantelesComponent,
+      },
+      {
+        path: 'pdf-example',
+        component:PdfExampleComponent ,
       },
       {
         path: 'frm-plantel',
