930aae700a6273c3e4f0b62e170d9d65b5acf7fd
diaz-rigo <morelosalfaro@gmail.com>
2025-01-09 04:49:47 -0600

    Update unauthorized.component.ts

--- src/app/modules/public/pages/unauthorized/unauthorized.component.ts
+++ src/app/modules/public/pages/unauthorized/unauthorized.component.ts
@@ -1,20 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID } from '@angular/core';
 
 @Component({
-  selector: 'app-unauthorized',
-  templateUrl: './unauthorized.component.html',
-  styleUrls: ['./unauthorized.component.scss'] // Corregido: styleUrl -> styleUrls
+    selector: 'app-unauthorized',
+    templateUrl: './unauthorized.component.html',
+    styleUrls: ['./unauthorized.component.scss'],
 })
 export class UnauthorizedComponent {
-  constructor(private location: Location, private router: Router) {}
+  constructor(
+    private location: Location,
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object // Inyección de PLATFORM_ID
+  ) {}
 
   goBack() {
-    if (window.history.length > 1) {
-      this.location.back(); // Navega a la página anterior si hay historial
+    if (isPlatformBrowser(this.platformId)) { // Verifica si el entorno es un navegador
+      if (window.history.length > 1) {
+        this.location.back(); // Navega a la página anterior
+      } else {
+        this.router.navigate(['/']); // Redirige al inicio
+      }
     } else {
-      this.router.navigate(['/']); // Redirige al inicio si no hay historial
+      console.warn('El objeto window no está disponible en este entorno. Redirigiendo al inicio.');
+      this.router.navigate(['/']);
     }
   }
 }
