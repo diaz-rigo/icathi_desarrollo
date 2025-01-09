90f93c8396e2ec2a267d9bdd400d42b9e9aa6c37
diaz-rigo <morelosalfaro@gmail.com>
2025-01-09 04:48:57 -0600

    Update coordinador-academico.component.ts

--- src/app/modules/coordinador-academico/coordinador-academico.component.ts
+++ src/app/modules/coordinador-academico/coordinador-academico.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-coordinador-academico',
-  templateUrl: './coordinador-academico.component.html',
-  styleUrl: './coordinador-academico.component.scss'
+    selector: 'app-coordinador-academico',
+    templateUrl: './coordinador-academico.component.html',
+    styleUrl: './coordinador-academico.component.scss',
+    standalone: false
 })
 export class CoordinadorAcademicoComponent {
 
