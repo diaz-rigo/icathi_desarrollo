474879aaa6fcf6bf03a984a199786c5d68d81aa4
diaz-rigo <morelosalfaro@gmail.com>
2025-01-09 01:35:15 -0600

    Update oferta-educativa.component.ts

--- src/app/modules/alumno/pages/oferta-educativa/oferta-educativa.component.ts
+++ src/app/modules/alumno/pages/oferta-educativa/oferta-educativa.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-oferta-educativa',
-    templateUrl: './oferta-educativa.component.html',
-    styleUrl: './oferta-educativa.component.scss',
-    standalone: false
+  selector: 'app-oferta-educativa',
+  templateUrl: './oferta-educativa.component.html',
+  styleUrl: './oferta-educativa.component.scss'
 })
 export class OfertaEducativaComponent {
 
