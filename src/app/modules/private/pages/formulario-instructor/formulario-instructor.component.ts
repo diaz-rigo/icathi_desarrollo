b8fdad434d0930c97ed00a71e0295cb82b424c1b
diaz-rigo <morelosalfaro@gmail.com>
2025-01-09 04:49:17 -0600

    Update formulario-instructor.component.ts

--- src/app/modules/private/pages/formulario-instructor/formulario-instructor.component.ts
+++ src/app/modules/private/pages/formulario-instructor/formulario-instructor.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-formulario-instructor',
-  templateUrl: './formulario-instructor.component.html',
-  styleUrl: './formulario-instructor.component.scss'
+    selector: 'app-formulario-instructor',
+    templateUrl: './formulario-instructor.component.html',
+    styleUrl: './formulario-instructor.component.scss',
+    standalone: false
 })
 export class FormularioInstructorComponent {
 
