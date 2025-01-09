e0cab5aa540e9a5325653b683d1ac2be1f0f3b01
diaz-rigo <morelosalfaro@gmail.com>
2025-01-09 04:49:44 -0600

    Update home.component.ts

--- src/app/modules/public/pages/home/home.component.ts
+++ src/app/modules/public/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'] // Corregido: styleUrls en plural
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'] // Corregido: styleUrls en plural
+    ,
+    standalone: false
 })
 export class HomeComponent {
 
