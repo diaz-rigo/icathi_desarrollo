9b1f11f7a4fa391373983ef31a32d175fbc29075
diaz-rigo <morelosalfaro@gmail.com>
2025-01-09 04:49:50 -0600

    Update validador.component.ts

--- src/app/modules/validador/validador.component.ts
+++ src/app/modules/validador/validador.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-validador',
-  templateUrl: './validador.component.html',
-  styleUrl: './validador.component.scss'
+    selector: 'app-validador',
+    templateUrl: './validador.component.html',
+    styleUrl: './validador.component.scss',
+    standalone: false
 })
 export class ValidadorComponent {
 
