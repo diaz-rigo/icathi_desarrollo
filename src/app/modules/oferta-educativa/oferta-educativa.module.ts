47ee434a235ef0c6ca577e365592c6186985b483
diaz-rigo <morelosalfaro@gmail.com>
2025-01-15 23:52:51 -0600

    Update oferta-educativa.module.ts

--- src/app/modules/oferta-educativa/oferta-educativa.module.ts
+++ src/app/modules/oferta-educativa/oferta-educativa.module.ts
@@ -1,17 +1,26 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OfertaEducativaRoutingModule } from './oferta-educativa-routing.module';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { OfertaEducativaComponent } from './oferta-educativa.component';
+import { HeaderComponent } from './component/header/header.component';
+import { ListadoCursosComponent } from './views/cursos/listado-cursos.component';
+import { CursoModalidadCAEComponent } from './views/cursos/typos-cursos/curso-modalidad-cae/curso-modalidad-cae.component';
+import { CursoModalidadEscuelaComponent } from './views/cursos/typos-cursos/curso-modalidad-escuela/curso-modalidad-escuela.component';
+import { CursoModalidadVirtualComponent } from './views/cursos/typos-cursos/curso-modalidad-virtual/curso-modalidad-virtual.component';
+  import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PdfExampleComponent } from './views/pdf-example/pdf-example.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [RouterModule,
-    CommonModule,
+  declarations: [PdfExampleComponent,
+    HomeComponent,CursoModalidadCAEComponent,CursoModalidadEscuelaComponent,CursoModalidadVirtualComponent,OfertaEducativaComponent, HeaderComponent,ListadoCursosComponent
+  ],  
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [RouterModule,HttpClientModule,
+    CommonModule,FormsModule ,ReactiveFormsModule,
     OfertaEducativaRoutingModule
   ]
 })
