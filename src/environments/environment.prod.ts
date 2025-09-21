c9cd114a0f195d169bb271eb5617840f8b687bde
meandro02 <rd209422@gmail.com>
2025-09-21 11:44:44 -0600

    prueba deplou

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,7 @@ export const environment = {
   production: true
 
       // ,api:'http://localhost:3000'
-      
 //  ,api:'https://iicathi-api.onrender.com'
-
 ,api:'https://iicathi-api-yiit.onrender.com'
 
 //
